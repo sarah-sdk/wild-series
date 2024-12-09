a7e9946345c7d45e92167f70b3a9e350ff9027af
Sarah Smandack <sarah.smandack@gmail.com>
2024-12-09 16:04:27 +0100

    finish step 04 - add categories

--- server/src/modules/say/sayActions.ts
+++ server/src/modules/say/sayActions.ts
@@ -1,7 +1,8 @@
 import type { RequestHandler } from "express";
 
-const sayWelcome: RequestHandler = (_req, res) => {
-  res.send("Welcome to Wild Series !");
+const sayWelcome: RequestHandler = (req, res) => {
+  console.info(req.query);
+  res.send(`Welcome to Wild Series, ${req.query.name} !`);
 };
 
 export default { sayWelcome };
--- server/src/router.ts
+++ server/src/router.ts
@@ -15,11 +15,19 @@ router.post("/api/items", itemActions.add);
 
 /* ************************************************************************* */
 
-import programActions from "./modules/program/programActions";
 import sayActions from "./modules/say/sayActions";
 
 router.get("/", sayActions.sayWelcome);
+
+import programActions from "./modules/program/programActions";
+
 router.get("/api/programs", programActions.browse);
+router.get("/api/programs/:id", programActions.read);
+
+import categoryActions from "./modules/category/categoryActions";
+
+router.get("/api/categories", categoryActions.browse);
+router.get("/api/categories/:id", categoryActions.read);
 
 /* ************************************************************************* */
 
