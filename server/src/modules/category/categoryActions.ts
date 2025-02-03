214414e3ff256eb090c84c6edde5ae8a5dd1748d
Sarah Smandack <sarah.smandack@gmail.com>
2025-02-03 16:34:17 +0100

    step from example

--- server/src/modules/category/categoryActions.ts
+++ server/src/modules/category/categoryActions.ts
@@ -1,6 +1,7 @@
 // Some data to make the trick
 
 import type { RequestHandler } from "express";
+import categoryRepository from "./categoryRepository";
 
 const categories = [
   {
@@ -15,8 +16,10 @@ const categories = [
 
 // Declare the actions
 
-const browse: RequestHandler = (req, res) => {
-  res.json(categories);
+const browse: RequestHandler = async (req, res) => {
+  const categoriesFromDB = await categoryRepository.readAll();
+
+  res.json(categoriesFromDB);
 };
 
 const read: RequestHandler = (req, res) => {
